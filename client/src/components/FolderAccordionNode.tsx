f01e2ad06c877fc9fbecb3e20149932d8e088a67
moshe.hasson <moshe.hasson@gmail.com>
2026-05-23 22:42:56 +0300

    green v

--- client/src/components/FolderAccordionNode.tsx
+++ client/src/components/FolderAccordionNode.tsx
@@ -68,6 +68,11 @@ export function FolderAccordionNode({ node, activePath, activeId, depth, onSelec
               <polyline points="12 6 12 12 16 14" />
             </svg>
           )}
+          {isApproved && (
+            <svg className="tree-node__lock tree-node__lock--approved" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" aria-label="Access granted">
+              <polyline points="20 6 9 17 4 12" />
+            </svg>
+          )}
         </Link>
       </div>
 
